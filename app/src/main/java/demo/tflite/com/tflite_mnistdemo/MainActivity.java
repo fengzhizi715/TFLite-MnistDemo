89af778e0d6cb50197ec799d58ee702a069bc8db
fengzhizi715 <fengzhizi715@126.com>
2018-11-07 15:25:33 +0800

    init the project

---
+++ app/src/main/java/demo/tflite/com/tflite_mnistdemo/MainActivity.java
@@ -0,0 +1,13 @@
+package demo.tflite.com.tflite_mnistdemo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
